75f46a5740309c893dc58b5c1c45bafe864b13a5
AdrianResende <adrianresende147@gmail.com>
2025-04-03 14:35:19 -0300

    feat: create UserClass path

---
+++ src/main/java/BackFinancas/demo/domain/User.java
@@ -0,0 +1,14 @@
+package BackFinancas.demo.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    private String usuario;
+    private String senha;
+}
